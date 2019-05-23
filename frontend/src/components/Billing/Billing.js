fdba1a3b5d41a6a7cb159598ce648e4078caa083
Melvin Merlos <melvinamerlos@gmail.com>
2019-05-22 23:11:01 -0400

    added billing navigation router link, billing page not yet built out, there ass a placeholder

---
+++ frontend/src/components/Billing/Billing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeaderContainer from "../HeaderComponents/HeaderContainer"
+
+function Billing() {
+  
+  
+  return (
+    <div className="trip-list-container">
+      <HeaderContainer />
+      <h1>•  Trip Planner  •</h1>
+      <div className="billing">
+        <p>Sorry, this page is currently unavailable. Please try again at a later time.</p>       
+      </div>
+    </div>
+  );
+}
+export default Billing;
