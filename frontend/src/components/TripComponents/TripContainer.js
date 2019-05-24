9572ddb5b644afefc62bbc1cf9826ffcf4c435c8
erscriber <erscriber@gmail.com>
2019-05-24 00:01:40 -0400

    Refactoring trip table to make it a sortable feature

--- frontend/src/components/TripComponents/TripContainer.js
+++ frontend/src/components/TripComponents/TripContainer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import HeaderContainer from '../HeaderComponents/HeaderContainer';
 import TripHeader from './TripHeader';
-import TripTable from './TripTable';
+//import TripTable from './TripTable';
+import TableTest from './TableTest'
 
 const TripContainer = () => {
     return (
         <div>
             <HeaderContainer />
             <TripHeader />
-            <TripTable />
+            <TableTest />
         </div>
     )
 };
