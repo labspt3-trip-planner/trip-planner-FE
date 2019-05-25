59db95c3c976499984d88ac8c595efc69b80cbc6
erscriber <erscriber@gmail.com>
2019-05-25 10:31:37 -0400

    Cleaned file structure; added a feature to the table

--- frontend/src/components/TripComponents/TripContainer.js
+++ frontend/src/components/TripComponents/TripContainer.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../HeaderComponents/HeaderContainer';
 import TripHeader from './TripHeader';
-//import TripTable from './TripTable';
-import TableTest from './TableTest'
+import TripTable from './TripTable'
 
 const TripContainer = () => {
     return (
         <div>
             <HeaderContainer />
             <TripHeader />
-            <TableTest />
+            <TripTable />
         </div>
     )
 };
