c90c5e8c7285ff8488e845be88d33f1101f0f8ce
Jonathan Dorety <kyullog@gmail.com>
2019-05-25 17:46:00 -0400

    Complete configuration file

--- frontend/src/components/Firebase/firebase.js
+++ frontend/src/components/Firebase/firebase.js
@@ -1,19 +1,26 @@
 import app from "firebase/app";
+import "firebase/auth";
 
+require("dotenv").config();
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
+
+    this.auth = app.auth();
   }
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
 }
 
 export default Firebase;
