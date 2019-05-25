50305e3ea91dee6d090532eb6d91af27156514a6
Melvin Merlos <melvinamerlos@gmail.com>
2019-05-25 15:49:32 -0400

    building billing page

--- frontend/src/components/Billing/Billing.js
+++ frontend/src/components/Billing/Billing.js
@@ -9,9 +9,30 @@ function Billing() {
     <div className="trip-list-container">
       <HeaderContainer />
       <h1 className="space">•  Billing  •</h1>
-      <div className="billing">
-        <p>Sorry, this page is currently unavailable. Please try again at a later time.</p>       
-      </div>
+      <section className="billing">
+        <div className="billing-option">
+          <h1>Trip Planner Standard</h1>
+          <ul>
+            <li>Save multiple trips with option for multiple destinations</li>
+            <li>Add lists to trip (check lists, or otherwise)</li>
+            <li>Pull data from API for restaurants/events in each destination and allow a user to favorite</li>
+            <li>Display map with 'itinerary' or pinpoints of favorited restaurants/events</li>
+            <li>Upgrade/billing feature, subscription gives user unlimited trips to save</li>
+          </ul>
+          </div>   
+        <div className="billing-option">
+          <h1>Trip Planner Pro</h1>
+          <ul>
+            <li>Save multiple trips with option for multiple destinations</li>
+            <li>Add lists to trip (check lists, or otherwise)</li>
+            <li>Pull data from API for restaurants/events in each destination and allow a user to favorite</li>
+            <li>Display map with 'itinerary' or pinpoints of favorited restaurants/events</li>
+            <li>Upgrade/billing feature, subscription gives user unlimited trips to save</li>
+            <li><b>Electron app for desktop version</b></li>
+            <li><b>calendar view itinerary</b></li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
