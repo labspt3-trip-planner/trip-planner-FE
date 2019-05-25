58c41d75a26ecdea4d5ca38d92d551efb21c8aff
Jonathan Dorety <kyullog@gmail.com>
2019-05-25 14:47:58 -0400

    Implement top level Firebase config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
+  <Router>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
+  </Router>,
+  document.getElementById("root")
 );
