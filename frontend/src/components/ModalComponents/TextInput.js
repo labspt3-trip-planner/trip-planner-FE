4e68682c4033f5ee4359c625792391c6ad70c723
erscriber <erscriber@gmail.com>
2019-05-31 21:07:12 -0400

    Refactor of Add Trip Modal

---
+++ frontend/src/components/ModalComponents/TextInput.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: 1,
+        marginRight: 1,
+    },
+}));
+
+function OutlinedTextFields() {
+    const classes = useStyles();
+
+
+    return (
+        <form className={classes.container} noValidate autoComplete="off">
+            <TextField 
+                id="outlined-full-width"
+                label="New Trip"
+                style={{ margin: 8 }}
+                placeholder="Trip Name..."
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+        </form>
+    );
+}
+
+export default OutlinedTextFields;
