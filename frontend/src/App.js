3004656e943fa03897a94a53adfe810afcae6935
Jonathan Dorety <kyullog@gmail.com>
2019-06-12 19:56:01 -0400

    Display google map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import Welcome from "./components/Login/Welcome";
-import TripContainer from './components/TripComponents/TripContainer';
-import TripInfoContainer from './components/TripInfoComponents/TripInfoContainer';
-import Signup from './components/Login/Signup';
-import Billing from './components/Billing/Billing';
-import { BrowserRouter as Switch, Route } from 'react-router-dom';
-import Map from "./components/Map/MapDisplay";
-
+import TripContainer from "./components/TripComponents/TripContainer";
+import TripInfoContainer from "./components/TripInfoComponents/TripInfoContainer";
+import Signup from "./components/Login/Signup";
+import Billing from "./components/Billing/Billing";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import GMap from "./components/Map/GMap";
 
 function App() {
-	return (
-		<Switch>
-			<Route exact path="/" component={Welcome} />
-			<Route path="/triplist" component={TripContainer} />
-			<Route path="/trip" component={TripInfoContainer} />
-			<Route path="/signup" component={Signup} />
-			<Route path="/billing" component={Billing} />
-			<Route path="/map" component={Map} />
-		</Switch>
-	)
+  return (
+    <Switch>
+      <Route exact path="/" component={Welcome} />
+      <Route path="/triplist" component={TripContainer} />
+      <Route path="/trip" component={TripInfoContainer} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/billing" component={Billing} />
+      <Route path="/map" render={props => <GMap isMarkerShown {...props} />} />
+    </Switch>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Map/GMap.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const GMap = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
+      process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    }&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <div>
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  </div>
+));
+
+export default GMap;
