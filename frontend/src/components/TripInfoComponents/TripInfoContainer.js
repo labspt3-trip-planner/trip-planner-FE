d34253ce52d2c824612f56a3245cc3233fa00e83
Jonathan Dorety <kyullog@gmail.com>
2019-06-12 22:41:26 -0400

    Componentize TripInfoContainer
    Gets trip info from back-end

--- frontend/src/components/TripInfoComponents/TripInfoContainer.js
+++ frontend/src/components/TripInfoComponents/TripInfoContainer.js
@@ -4,19 +4,42 @@ import "../TripComponents/Page.css";
 import CheckListContainer from "./CheckListComponents/CheckListContainer";
 import Title from "./TripName";
 import GMap from "../Map/GMap";
+import { axios } from "../Axios";
 
-const TripInfoContainer = () => {
-  return (
-    <div className="page-container">
-      <HeaderContainer />
-      <Title />
-      <br />
-      <CheckListContainer />
-      <div className="map-container">
-        <GMap isMarkerShown />
+class TripInfoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tripName: "",
+      destinations: [],
+      currentDestination: {},
+      tripId: this.props.match.params.tripId,
+      favorites: [],
+      participants: [],
+      flight_info: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`/trip/${this.state.tripId}`)
+      .then(res => this.setState({ ...res.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="page-container">
+        <HeaderContainer />
+        <Title tripName={this.state.tripName} />
+        <br />
+        <CheckListContainer />
+        <div className="map-container">
+          <GMap isMarkerShown />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TripInfoContainer;
