d5af9179e13ef57c2374cedeeaed2c7841296d86
Jesse Theisen <theiseja@gmail.com>
2019-07-10 21:10:23 -0500

    In the process of working on the frontend portion of Stripe. Saving progress

--- frontend/src/components/Billing/Stripe.js
+++ frontend/src/components/Billing/Stripe.js
@@ -5,8 +5,29 @@ import StripeCheckout from 'react-stripe-checkout';
 const PAYMENT_SERVER_URL = '3RD_PARTY_SERVER';
 const CURRENCY = 'USD';
 
-function Stripe() {
-  
-  }
+const successPayment = data => {
+  alert('Payment Successful');
+};
 
-  export default Stripe;
+const errorPayment = data => {
+  alert('Error accepting payment');
+};
+
+const onToken = (description) => token => 
+axios.post(PAYMENT_SERVER_URL,
+  {
+    description,
+    source: token.id,
+    currency: CURRENCY
+  })
+  .then(successPayment)
+  .catch(errorPayment);
+
+  const Checkout = ({ name, description, currency}) => [
+    <StripeCheckout
+    name={name}
+    description={description}
+    currency={currency}
+    stripeKey={}
+   />
+  ]
