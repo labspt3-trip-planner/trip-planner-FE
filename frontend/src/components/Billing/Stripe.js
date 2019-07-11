0277efb61d5316d1efb9e8702355da5ad4d047d1
Jesse Theisen <theiseja@gmail.com>
2019-07-10 22:45:38 -0500

    Saving Progress

--- frontend/src/components/Billing/Stripe.js
+++ frontend/src/components/Billing/Stripe.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+
+const stripeButton = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
  
 export default class Stripe extends React.Component {
+
   onToken = (token) => {
     fetch('http://localhost:9090/checkout', {
       method: 'POST',
@@ -13,15 +25,15 @@ export default class Stripe extends React.Component {
     });
   }
  
-  // ...
- 
   render() {
+    const classes = stripeButton();
     return (
-      // ...
+      <Button variant="contained" color='primary' className={classes.button}>
       <StripeCheckout
         token={this.onToken}
         stripeKey="STRIPE_PUBLISHABLE_KEY"
       />
+      </Button>
     )
   }
 }
