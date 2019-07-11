40a84edef17c0e9fbda17940dae0ab56066c3538
Jesse Theisen <theiseja@gmail.com>
2019-07-10 22:19:45 -0500

    Update Stripe.js

--- frontend/src/components/Billing/Stripe.js
+++ frontend/src/components/Billing/Stripe.js
@@ -1,32 +1,27 @@
-import axios from 'axios';
-import React from 'react';
+import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
-
-const PAYMENT_SERVER_URL = '3RD_PARTY_SERVER';
-const CURRENCY = 'USD';
-
-const successPayment = data => {
-  alert('Payment Successful');
-};
-
-const errorPayment = data => {
-  alert('Error accepting payment');
-};
-
-const onToken = (description) => token => 
-axios.post(PAYMENT_SERVER_URL,
-  {
-    description,
-    source: token.id,
-    currency: CURRENCY
-  })
-  .then(successPayment)
-  .catch(errorPayment);
-
-  const Checkout = ({ name, description, currency}) => [
-    <StripeCheckout
-    name={name}
-    description={description}
-    currency={currency}
-   />
-  ]
+ 
+export default class Stripe extends React.Component {
+  onToken = (token) => {
+    fetch('http://localhost:9090/checkout', {
+      method: 'POST',
+      body: JSON.stringify(token),
+    }).then(response => {
+      response.json().then(data => {
+        alert(`Thank you for your payment, ${data.email}`);
+      });
+    });
+  }
+ 
+  // ...
+ 
+  render() {
+    return (
+      // ...
+      <StripeCheckout
+        token={this.onToken}
+        stripeKey=" STRIPE_PUBLISHABLE_KEY"
+      />
+    )
+  }
+}
---
+++ frontend/src/components/Billing/stripeConfig.js
@@ -0,0 +1,6 @@
+const stripeConfig = {
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    STRIPE_TEST_KEY: process.env.STRIPE_TEST_KEY
+  };
+
+  export default stripeConfig;
