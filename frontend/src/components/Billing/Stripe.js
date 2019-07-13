1f5bf66a4d632f3ea4667390507de28e731d3189
Jesse Theisen <theiseja@gmail.com>
2019-07-13 16:15:14 -0500

    Saving progress

--- frontend/src/components/Billing/Stripe.js
+++ frontend/src/components/Billing/Stripe.js
@@ -1,42 +1,22 @@
 import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/styles';
+import StripeCheckout from 'react-stripe-checkout'
 
+export default class Checkout extends React.Component {
+  
 
-// component can be moved if needed. I assumed Billing would be best
-
-const stripeButton = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-}));
- 
-export default class Stripe extends React.Component {
-
-  onToken = (token) => {
-    fetch('https://labspt3-trip-planner.herokuapp.com/payments/checkout', {
-      method: 'POST',
-      body: JSON.stringify(token),
-    }).then(response => {
-      response.json().then(data => {
-        alert(`Thank you for your payment, ${data.email}`);
-      });
-    });
-  }
- 
   render() {
-    const classes = stripeButton();
     return (
-      <Button variant="contained" color='primary' className={classes.button}>
       <StripeCheckout
-        token={this.onToken}
+        amount="9999"
+        billingAddress
+        description="Premium plan"
+        image="http://clipart-library.com/data_images/46219.jpg"
+        locale="auto"
+        name="Trip Planner"
         stripeKey="STRIPE_PUBLISHABLE_KEY"
+        token={this.onToken}
+        zipCode
       />
-      </Button>
     )
   }
 }
