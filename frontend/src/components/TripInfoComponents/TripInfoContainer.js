227d1e3cf7c6d0296357efe4b1f7493b9f494f65
Jonathan Dorety <kyullog@gmail.com>
2019-07-16 21:02:50 -0700

    Create initial faves list.  Needs polish / styling

--- frontend/src/components/TripInfoComponents/TripInfoContainer.js
+++ frontend/src/components/TripInfoComponents/TripInfoContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeaderContainer from "../HeaderComponents/HeaderContainer";
 import "../TripComponents/Page.css";
 import CheckListContainer from "./CheckListComponents/CheckListContainer";
+import FaveList from "../Favorites/FaveList";
 import Title from "./TripName";
 import GMap from "../Map/GMap";
 import { axios } from "../Axios";
@@ -69,6 +70,7 @@ class TripInfoContainer extends React.Component {
             favorites={this.state.favorites}
           />
         </div>
+        <FaveList favorites={this.state.favorites} />
       </div>
     );
   }
