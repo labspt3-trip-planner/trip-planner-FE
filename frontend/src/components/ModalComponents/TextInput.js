349397ff65b0adf6864a2dc4891667505336caec
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-07-23 21:15:07 -0400

    This commit should add hover styling to rows in Trips page desktop component

--- frontend/src/components/ModalComponents/TextInput.js
+++ frontend/src/components/ModalComponents/TextInput.js
@@ -1,40 +1,50 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import TextField from "@material-ui/core/TextField";
+// import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  textField: {
-    marginLeft: 1,
-    marginRight: 1
-  }
+	container: {
+		display: "flex",
+		flexWrap: "wrap"
+	},
+	textField: {
+		marginLeft: 1,
+		marginRight: 1
+	}
 }));
 
 function OutlinedTextFields(props) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        name="tripName"
-        value={props.nameValue}
-        id="outlined-full-width"
-        onChange={props.nameHandler}
-        label="New Trip"
-        style={{ margin: 8 }}
-        placeholder="Trip Name..."
-        fullWidth
-        margin="normal"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-    </form>
-  );
+	return (
+		<form className={classes.container} noValidate autoComplete="off">
+			<input
+				id="login-modal"
+				className="input"
+				type="text"
+				name="tripName"
+				maxLength="35"
+				placeholder="Trip Name"
+				value={props.nameValue}
+				onChange={props.nameValue}
+				label="New Trip"
+
+				// name="tripName"
+				// value={props.nameValue}
+				// id="outlined-full-width"
+				// onChange={props.nameHandler}
+				// label="New Trip"
+				// style={{ margin: 8 }}
+				// placeholder="Trip Name..."
+				// fullWidth
+				// margin="normal"
+				// variant="outlined"
+				// InputLabelProps={{
+				// 	shrink: true
+				// }}
+			/>
+		</form>
+	);
 }
 
 export default OutlinedTextFields;
