7b1a6f7ee6e904f8d055984b4ab9cd324f44dad9
Jonathan Dorety <kyullog@gmail.com>
2019-07-30 18:17:42 -0700

    Prettier format changes

--- frontend/src/components/TripInfoComponents/CheckListComponents/CheckListContainer.js
+++ frontend/src/components/TripInfoComponents/CheckListComponents/CheckListContainer.js
@@ -1,65 +1,63 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import CheckListForm from './CheckListForm';
-import Checklist from './Checklist';
-import TodoListForm from './TodoListForm';
-import TodoList from './TodoList';
-import useChecklistState from './useChecklistState';
-import useTodoState from './useTodoState';
-import './trip-page.css';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import CheckListForm from "./CheckListForm";
+import Checklist from "./Checklist";
+import TodoListForm from "./TodoListForm";
+import TodoList from "./TodoList";
+import useChecklistState from "./useChecklistState";
+import useTodoState from "./useTodoState";
+import { connect } from "react-redux";
+import { updateList } from "../../../store/actions/listActions";
+import "./trip-page.css";
 
-const Checklists = () => {
-    const { lists, addList, deleteList } = useChecklistState([]);
-    const { todos, addTodo, deleteTodo  } = useTodoState([]);
+const Checklists = props => {
+  const { lists, addList, deleteList } = useChecklistState([]);
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
 
-    return (
-        <div className="lists-wrapper">
-            <p className="list-label">Pre-Trip</p>
-            <div className="lists-section">
-                <div className="list-wrapper">
-                    <Typography component="h4" variant="h5">
-                        Packing List
-                    </Typography>
+  return (
+    <div className="lists-wrapper">
+      <p className="list-label">Pre-Trip</p>
+      <div className="lists-section">
+        <div className="list-wrapper">
+          <Typography component="h4" variant="h5">
+            Packing List
+          </Typography>
 
-                    <CheckListForm 
-                        saveList={listText => {
-                            const trimmedText = listText.trim();
+          <CheckListForm
+            saveList={listText => {
+              const trimmedText = listText.trim();
 
-                            if(trimmedText.length > 0) {
-                                addList(trimmedText);
-                            }
-                        }}
-                    />
+              if (trimmedText.length > 0) {
+                addList(trimmedText);
+              }
+            }}
+          />
 
-                    <Checklist 
-                        lists={lists}
-                        deleteList={deleteList}
-                    />
-                </div>
-                <div className="list-wrapper">
-                    <Typography component="h4" variant="h5">
-                        To Do List
-                    </Typography>
-
-                    <TodoListForm 
-                        saveTodo={todoText => {
-                            const trimmedTodoText = todoText.trim();
-
-                            if(trimmedTodoText.length > 0) {
-                                addTodo(trimmedTodoText);
-                            }
-                        }}
-                    />
-
-                    <TodoList 
-                        todos={todos}
-                        deleteTodo={deleteTodo}
-                    />
-                </div>
-            </div>
+          <Checklist lists={lists} deleteList={deleteList} />
         </div>
-            
-    );
+        <div className="list-wrapper">
+          <Typography component="h4" variant="h5">
+            To Do List
+          </Typography>
+
+          <TodoListForm
+            saveTodo={todoText => {
+              const trimmedTodoText = todoText.trim();
+
+              if (trimmedTodoText.length > 0) {
+                addTodo(trimmedTodoText);
+              }
+            }}
+          />
+
+          <TodoList todos={todos} deleteTodo={deleteTodo} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Checklists;
+export default connect(
+  null,
+  { updateList }
+)(Checklists);
--- frontend/src/components/TripInfoComponents/CheckListComponents/CheckListForm.js
+++ frontend/src/components/TripInfoComponents/CheckListComponents/CheckListForm.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import useInputState from './useInputState';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import useInputState from "./useInputState";
+import { connect } from "react-redux";
+import { updateList } from "../../../store/actions/listActions";
 
 const ListForm = ({ saveList }) => {
-    const { value, reset, onChange } = useInputState('');
-
-    return (
-        <form
-            onSubmit={event => {
-                event.preventDefault();
-                saveList(value);
-                reset();
-            }}
-        >
-            <TextField 
-                variant="outlined"
-                placeholder="Add another..."
-                margin="normal"
-                onChange={onChange}
-                value={value}
-            />
-        </form>
-    );
+  const { value, reset, onChange } = useInputState("");
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        saveList(value);
+        
+        reset();
+      }}
+    >
+      <TextField
+        variant="outlined"
+        placeholder="Add another..."
+        margin="normal"
+        onChange={onChange}
+        value={value}
+      />
+    </form>
+  );
 };
 
-export default ListForm;
+export default connect(
+  null,
+  { updateList }
+)(ListForm);
