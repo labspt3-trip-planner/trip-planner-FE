09226341b0d2d5ba0aaeaaf582be8a82bdac3f37
Jesse Theisen <theiseja@gmail.com>
2019-07-30 21:07:46 -0500

    Saving progress for reducers, packing list partially complete

--- frontend/src/store/reducers/packingReducer.js
+++ frontend/src/store/reducers/packingReducer.js
@@ -34,64 +34,64 @@ import {
           ...state,
           isFetchingList: true,
         };
-      case LISTS:
+      case PACKING:
         return {
           ...state,
           isFetchingList: false,
           lists: action.payload,
         };
-      case LISTS_ERROR:
+      case PACKING_ERROR:
         return {
           ...state,
           isFetchingList: false,
           listError: action.payload,
         };
-      case CREATE_LIST:
+      case CREATE_PACKING_LIST:
         return {
           ...state,
           isCreatingList: true,
         };
-      case LIST_CREATED:
+      case PACKING_LIST_CREATED:
         return {
           ...state,
           isCreatingList: false,
           lists: [...this.state.lists, action.payload],
         };
-      case LIST_ERROR:
+      case PACKING_LIST_ERROR:
         return {
           ...state,
           isCreatingList: false,
           listError: action.payload,
         };
-      case EDIT_LIST:
+      case EDIT_PACKING_LIST:
         return {
           ...state,
           isEditingList: true,
         };
-      case LIST_EDITED:
+      case PACKING_LIST_EDITED:
         return {
           ...state,
           isEditingList: false,
           lists: this.state.lists.map(list => (list.id === action.payload.id ? { ...action.payload } : list)),
         };
-      case EDIT_ERROR:
+      case PACKING_EDIT_ERROR:
         return {
           ...state,
           isEditingList: false,
           listError: action.payload,
         };
-      case DELETE_LIST:
+      case DELETE_PACKING_LIST:
         return {
           ...state,
           isDeletingList: true,
         };
-      case LIST_DELETED:
+      case PACKING_LIST_DELETED:
         return {
           ...state,
           isDeletingLIST: false,
           lists: this.state.lists.filter(list => list.id !== action.payload.id),
         };
-      case DELETE_ERROR:
+      case DELETE_PACKING_ERROR:
         return {
           ...state,
           isDeletingList: false,
