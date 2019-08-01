01706a131fdc9a8f4cfd8517ea7fec6ab319db9e
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-07-31 22:34:02 -0400

    This commit should practically finish the new trip modal render

--- frontend/src/components/ModalComponents/Modal.js
+++ frontend/src/components/ModalComponents/Modal.js
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom"; // Not in use, just cleaning errors
 import "./modal.css";
-import "../TripComponents/Page.css";
 import PropTypes from "prop-types";
 
 export default class Modal extends Component {
 	onClose = e => {
 		this.props.onClose && this.props.onClose(e);
 	};
+
 	render() {
+		console.log("modaljs", this.props.children);
 		if (!this.props.show) {
 			return null;
 		}
 		return (
 			<div className="modal-container">
-				<div className="content">{this.props.children}</div>
+				<div className="content">{this.props.children[0]}</div>
 				<div className="actions">
-					<button id="modal-button" onClick={this.props.addTrip}>
-						Add Trip
-					</button>
-					<button id="modal-button" onClick={e => this.onClose(e)}>
-						Close
-					</button>
+					<h2 className="actions-h2"> Create Trip</h2>
+					<div className="props-area">
+						<div className="input-box">
+							{this.props.children[1]}
+						</div>
+						<div>{this.props.children[2]}</div>
+						<div className="date-input-area">
+							{this.props.children[3]}
+						</div>
+					</div>
+					<div className="modal-button-area">
+						<button
+							id="modal-button"
+							onClick={e => this.onClose(e)}
+						>
+							Close
+						</button>
+						<button id="modal-button" onClick={this.props.addTrip}>
+							Add Trip
+						</button>
+					</div>
 				</div>
 			</div>
 		);
